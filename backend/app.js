d1e8f60bdb557dfd8116034405af697e0c9f057a
Julian Poyourow <julianpoyo@gmail.com>
2015-11-07 01:24:01 -0800

    Added gridFS and busboy body parser

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var busboyBodyParser = require('busboy-body-parser');
 
 //Database
 var mongo = require('mongodb');
@@ -25,8 +26,9 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(busboyBodyParser({limit: '50mb'}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
